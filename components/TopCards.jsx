75f551cf9158ca4f81dad619fe11b0dd3e1e3d18
Nufdha <mmfnufdha@gmail.com>
2024-07-11 21:41:59 +0530

    Update TopCards.jsx

--- components/TopCards.jsx
+++ components/TopCards.jsx
@@ -4,18 +4,19 @@ import { FaUserAlt } from "react-icons/fa";
 const TopCards = () => {
   return (
     <div className='grid lg:grid-cols-5 gap-4 p-4'>
-      <div className='lg:col-span-2 col-span-1 bg-custom-purple text-white flex justify-between items-center w-full p-4 rounded-lg h-24 lg:h-32'>
-        <div className='bg-custom-gray h-12 w-12 flex items-center justify-center text-white p-2 rounded-lg'>
-          <FaUserAlt className='w-7 h-7' />
+      <div className='lg:col-span-2 col-span-1 bg-custom-purple text-white flex justify-between items-center p-4 rounded-lg h-24 lg:h-32 w-custom-width'>
+        <div className='bg-custom-gray h-12 w-12 flex items-center justify-center text-white p-3 rounded-full'>
+          <FaUserAlt className='w-6 h-6' />
         </div>
-        <div className='flex flex-col w-full'>
-          <div>System users</div>
+        <div className='flex flex-col w-full items-end'>
+          <div className='ml-3 text-5xl font-bold'>62</div>
+          <div>System Users</div>
         </div>
       </div>
-      <div className='lg:col-span-2 col-span-1 bg-custom-purple text-white flex justify-between items-center w-full p-4 rounded-lg h-24 lg:h-32'>
+      <div className='lg:col-span-2 col-span-1 bg-custom-purple text-white flex justify-between items-center p-4 rounded-lg h-24 lg:h-32 w-custom-width'>
         Teams
       </div>
-      <div className='bg-custom-purple text-white flex justify-between items-center w-full p-4 rounded-lg h-24 lg:h-32'>
+      <div className='bg-custom-purple text-white flex justify-between items-center p-4 rounded-lg h-24 lg:h-32 w-custom-width'>
         Projects
       </div>
     </div>
