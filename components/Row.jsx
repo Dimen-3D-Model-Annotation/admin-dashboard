a987aa1ecfcfba0560af37e8d1bd886d43c45880
Nufdha <mmfnufdha@gmail.com>
2024-07-27 16:50:11 +0530

    Update Row.jsx

--- components/Row.jsx
+++ components/Row.jsx
@@ -5,8 +5,9 @@ const Row = ({ row }) => {
     <div>
       {row.map((item) => (
         <div key={item.email} className="rowdiv flex justify-between items-center">
-          <p className="text-left">{item.name}</p>
-          <p className="text-right">{item.email}</p>
+          <img src={item.image} alt={item.name} className="w-8 h-8 rounded-full mr-4" />
+          <p className="text-left flex-1">{item.name}</p>
+          <p className="text-right flex-1">{item.email}</p>
         </div>
       ))}
     </div>
