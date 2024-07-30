468bcb5c46f0ab5dd4f17b4de788e8d30cc0c634
Nufdha <mmfnufdha@gmail.com>
2024-07-30 10:11:22 +0530

    Update Box.jsx

--- components/Box.jsx
+++ components/Box.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from "@components/Button"; 
+import Button from '@components/Button';
 
 const Box = ({ box, isDetailBox = false, isPremiumPlan = false }) => {
   const width = "w-120"; // Updated width for both heading and details
@@ -11,16 +11,16 @@ const Box = ({ box, isDetailBox = false, isPremiumPlan = false }) => {
       {isDetailBox ? (
         <>
           {box.map((item, index) => (
-            <div key={index} className="flex items-center justify-between mb-6 m-3"> {/* Increased margin-bottom */}
+            <div key={index} className="flex items-center justify-between mb-6 m-3">
               <p className="text-left font-bold text-white">{item.name}</p>
-              <div className="w-32 h-8 flex items-center justify-center bg-custom-gray text-white rounded-full"> {/* Box for numbers */}
+              <div className="w-32 h-8 flex items-center justify-center bg-custom-gray text-white rounded-full">
                 {isPremiumPlan ? (
                   <span className="font-bold">{item.number}</span>
                 ) : (
                   <>
-                    <span className="mr-3">-</span> {/* Minus icon */}
+                    <span className="mr-3">-</span>
                     <span className="font-bold">{item.number}</span>
-                    <span className="ml-3">+</span> {/* Plus icon */}
+                    <span className="ml-3">+</span>
                   </>
                 )}
               </div>
