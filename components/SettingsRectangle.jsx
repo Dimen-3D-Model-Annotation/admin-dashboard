6b36434862f37634d0ae4cf497f42023339aa6c9
Nufdha <mmfnufdha@gmail.com>
2024-07-31 09:24:43 +0530

    Update SettingsRectangle.jsx

--- components/SettingsRectangle.jsx
+++ components/SettingsRectangle.jsx
@@ -11,32 +11,28 @@ const Rectangles = () => {
       className={`relative ${width} ${height} ${borderRadius} bg-custom-togglegray cursor-pointer p-4`}
     >
       {/* User Image */}
-      <div className="absolute inset-y-14 left-0 flex items-left justify-center w-1/2">
+      <div className="flex justify-center mb-4">
         <img
           src="/assets/images/user1.webp"
           alt="User"
-          className="w-48 h-48 rounded-full border-4 border-white"
+          className="w-40 h-40 rounded-full border-4 border-white"
         />
       </div>
   
       {/* User Details */}
-      <div className="ml-40 mb-15 flex flex-col space-y-4 text-right">
-        <div>
-          <h2 className=" ml-60 text-white text-left">User Name</h2>
-          <div className="ml-60 bg-white rounded-lg p-2 text-black text-left w-80">John Doe</div> {/* Decreased width */}
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex flex-col items-start space-y-2">
+          <h2 className="text-white text-left">User Name</h2>
+          <div className="bg-white rounded-lg p-2 text-black w-80 text-left">John Doe</div>
         </div>
-        <div>
-          <h2 className=" ml-60 text-white text-left">Email</h2>
-          <div className="ml-60 bg-white rounded-lg p-2 text-black text-left w-80">john.doe@example.com</div> {/* Decreased width */}
+        <div className="flex flex-col items-start space-y-2">
+          <h2 className="text-white text-left">Email</h2>
+          <div className="bg-white rounded-lg p-2 text-black w-80 text-left">john.doe@example.com</div>
         </div>
-        <div>
-          <h2 className="ml-60 text-white text-left">Bio</h2>
-          <div className="ml-60 bg-white rounded-lg p-2 text-black text-left w-80">I'm a passionate 3D Designer.</div> {/* Decreased width */}
+        
+        <div className="flex flex-col items-start space-y-2">
+          <h2 className="text-blue-400 text-left">Change Password</h2>
         </div>
-        <div>
-          <h2 className="ml-96 text-blue-400 text-left">Change Password?</h2>
-         </div>
-
       </div>
     </div>
   );
